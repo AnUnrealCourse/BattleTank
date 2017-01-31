3afeea4c27947d5318b83407fd550eda31b8300c
An Doan <minh.an.doan@gmail.com>
2017-01-30 21:56:45 -0800

    Virtual and overide methods, created BeginPlay method that overrides from the AActor method.

--- BattleTank/Source/BattleTank/Private/TankPlayerController.cpp
+++ BattleTank/Source/BattleTank/Private/TankPlayerController.cpp
@@ -3,10 +3,17 @@
 #include "BattleTank.h"
 #include "TankPlayerController.h"
 
+void ATankPlayerController::BeginPlay()
+{
+	Super::BeginPlay();
+	UE_LOG(LogTemp, Warning, TEXT("TankPlayerController BeginPlay called!"));
+}
 
 ATank* ATankPlayerController::GetControlledTank() const
 {
 	return Cast<ATank>(GetPawn());
 }
+
+
 
 
