3e1abd255199ace3198c1a37e9f42606a6d696cd
An Doan <minh.an.doan@gmail.com>
2017-01-31 23:21:42 -0800

    Logging out possessed tank and finding player tank from TankAIController.

---
+++ BattleTank/Source/BattleTank/Private/TankAIController.cpp
@@ -0,0 +1,34 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "BattleTank.h"
+#include "TankAIController.h"
+
+void ATankAIController::BeginPlay()
+{
+	Super::BeginPlay();
+
+	auto PlayerTank = GetPlayerTank();
+
+	if (!PlayerTank)
+	{
+		UE_LOG(LogTemp, Warning, TEXT("TankAIController can't find tank."));
+	}
+	else {
+		UE_LOG(LogTemp, Warning, TEXT("TankAIController found player: %s."), *PlayerTank->GetName());
+	}
+}
+
+ATank* ATankAIController::GetControlledTank() const
+{
+	return Cast<ATank>(GetPawn());
+}
+
+ATank* ATankAIController::GetPlayerTank() const
+{
+	auto PlayerPawn = GetWorld()->GetFirstPlayerController()->GetPawn();
+
+	if (!PlayerPawn) { return nullptr; }
+	else {
+		return Cast<ATank>(PlayerPawn);
+	}
+}
